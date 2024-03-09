c1d03469ef34611bcc2d96f07657cd3f91670d70
Alexsandr Bikmeev <shadowfareed@yandex.ru>
2024-03-09 21:51:10 +0300

    small change and added files

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
 #продолжите решение здесь
-import xml.etree.ElementTree as ET
-tree = ET.parse('inventory.xml')
-root = tree.getroot()
-total = 0
-for item in root.findall('./ценности/предмет'):
-    total += int(item.find('ценность').text) * int(item.find('количество').text)
-print(total)
