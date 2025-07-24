a2f7d50e2c9da219c20d9f83cce99a33cc958de6
Alexsandr Bikmeev <shadowfareed@yandex.ru>
2025-07-24 21:59:56 +0300

    home change

--- _Парсинг/parser 2.py
+++ _Парсинг/parser 2.py
@@ -25,7 +25,7 @@ def info_players(side):
         print(f"{player['hero']:20} - {player['role']:15} / {player['kda']} - {str(player['gold']) + 'k':10} / {player['line']:20} / {player['name']:30} / {player['aspect']}")
 
 # SITE ---------------------------------------------------------------------------------------------------------------------------------------
-# code="8385995426"
+# code="8387154144"
 
 # driver = webdriver.Chrome()
 
